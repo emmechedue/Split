061c68baf25919fc279772fec0627f6c92e4937b
Stefano Duca <emmechedue@gmail.com>
2013-01-28 16:58:54 +0100

    i'm out

--- Constants.cpp
+++ Constants.cpp
@@ -8,15 +8,15 @@ using namespace std;
 Constants::Constants(){
 	N0=4; //Initial number of bacteria in the cell
 	x0=0.5;//Initial fraction of cooperators in the cell
-	T=4.; //Time when the simulation stops
-	interval=0.005; //Time step for which I print my results in fast
+	T=2; //Time when the simulation stops
+	interval=0.25; //Time step for which I print my results in fast
 	intervalens=0.001; //Time step for which I print my results in ensamble.txt
 	s=0.05; //Selection's strenght
 	p=10.; //Cooperators advantage
 	K=100.; //Carrying capacity
 	N_max=80; //The number of bacteria in the cell s.t. the cell splits
 	M_max=1000; //The maximum number of cells
-	N_loop=1000; //The number of times I iterate
+	N_loop=100; //The number of times I iterate
 }
 
 Constants::~Constants(){}
