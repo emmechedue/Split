97a3dda7784544cc95bdf42e9f565519c32d115f
Stefano Duca <emmechedue@gmail.com>
2013-03-19 15:15:23 +0100

    readded dothetimeanalysis

---
+++ dothetimesanalysis.py
@@ -0,0 +1,56 @@
+import numpy
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import matplotlib.colors as mc
+import matplotlib.mlab as mlab
+from matplotlib.ticker import NullFormatter
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
+import matplotlib.path as mpath
+import matplotlib.lines as mlines
+from pylab import *
+from numpy import array
+from configobj import ConfigObj
+
+#****************************************Let's first of all get all the parameters:
+config = ConfigObj("./parameters.txt")
+
+dummy = config["N0"]
+N0=int(dummy)
+
+dummy = config["x0"]
+x0=float(dummy)
+
+dummy = config["T"]
+T=float(dummy)
+
+dummy = config["interval"]
+interval=float(dummy)
+
+dummy = config["s"]
+s=float(dummy)
+
+dummy = config["p"]
+p=float(dummy)
+
+dummy = config["K"]
+K=float(dummy)
+
+dummy = config["N_max"]
+N_max=int(dummy)
+
+dummy = config["M_max"]
+M_max=int(dummy)
+
+dummy = config["choice"]
+cho=int(dummy)
+if cho==1:
+	choice="propagule"
+else:
+	choice="random splitting"
+	
+#***************************************************************************************
+
+#*****************************Loading everything and computing all the quantities I need
+# Here I load everything
