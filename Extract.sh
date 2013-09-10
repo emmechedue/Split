f0855663f5077e4d668f97e6b289887a4b57bfac
Stefano Duca <emmechedue@gmail.com>
2013-09-10 12:01:14 +0200

    added the shell script to extract

---
+++ Extract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NUMJOB=36278   # Here I have to give the script the number of the job!
+
+for i in $(seq 1 1 2)
+do
+	esse=`echo $i \\* 0.05 | bc`		#Computing the value of s
+	dummy=$(echo "$esse < 1" | bc)		#Here I create the folder named s+... or s+0....
+	if [ $dummy -eq 1 ]; then
+		DIR=s+0$esse
+	else
+		DIR=s+$esse
+	fi
+	mkdir -p $DIR
+	tar -xf job$NUMJOB.$i.tgz -C ./$DIR		#Unzipping the file
+	mv ./$DIR/data/Stefano.Duca/job$NUMJOB.$i/ensambleN.txt ./$DIR/data/Stefano.Duca/job$NUMJOB.$i/ensamblex.txt ./$DIR/data/Stefano.Duca/job$NUMJOB.$i/parameters.txt ./$DIR/data/Stefano.Duca/job$NUMJOB.$i/time.txt ./$DIR		#Putting the files in the proper folder and deleting everything that I don't need
+	rm -rf ./$DIR/data	
+done
+
+
+
+
+
+
