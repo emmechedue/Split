f8c23a8735aef11b0698f7a10938ed6d641a67af
Stefano Duca <emmechedue@gmail.com>
2013-10-15 14:08:28 +0200

    added the moran things

---
+++ Headers/Moran.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<cstdio>
+#include<cstdlib>
+#include<fstream>
+#include<math.h>
+#include<Hevolve.h>
+#include<gsl/gsl_rng.h>
+#include<Constants.h>
+#include<gsl_randist.h>
+
+using namespace std;
+
+//In this header I can decide how many groups have to be cooperative!
+
+const double num_coop=3;
