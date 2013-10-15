2ef4b9c415ec153056006cb2355b5368585aaf93
Stefano Duca <emmechedue@gmail.com>
2013-10-15 14:53:13 +0200

    started doing that

--- Headers/Moran.h
+++ Headers/Moran.h
@@ -14,19 +14,26 @@ using namespace std;
 
 const double num_coop=3; //This is the number of groups that start as cooperative! All the M_max - num_coop will start as defectors!
 
-void initializeNandx(int M_max, double *x, double *N){ //Here I initialize all the cells to start with 40 agents. Ther first num_coop groups will be cooperative, the others defectors!
+void initializeNandx(double *x, double *Nc, double *Nd, Constants cons{ //Here I initialize all the cells to start with 40 agents. Ther first num_coop groups will be cooperative, the others defectors!
 	int i;
 	
 	for(i=0;i < num_coop; i++){ //Initialized the cooperative groups!
 		x[i]=1.;
-		N[i]=40.;
+		Nc[i]=40.;
+		Nd[i]=0.;
 	}
-	for(i=num_coop; i<M_max;i++){ //Initializing all the rest of groups to defective
+	for(i=num_coop; i<cons.M_max;i++){ //Initializing all the rest of groups to defective
 		x[i]=0.;
-		N[i]=40.;
+		Nd[i]=40.;
+		Nc[i]=0.;
 	}
 	
 	return;
 }
 
-
+void initializeallgammas(double **G, double *Gamma,double *Nc, double *Nd, double *x, Constants cons){
+	int i,j; // i is going to be the group index and j the reaction index 
+	
+	//Here I should start calling intializegamma and then continue with all the others with 2 for loops!!
+	
+	
